0a811e8715fe83455394c3359db76ebca8a2c91a
PranathiBadana <pranathibadana414@gmail.com>
2026-09-08 10:44:47 +0530

    Add files via upload

--- Practice/app.js
+++ Practice/app.js
@@ -1,78 +1,19 @@
-// Importing built-in Node.js modules
-const os = require("os");
-const path = require("path");
-const dns = require("dns");
-const net = require("net");
+const express = require("express");
 
-// ==========================================
-// 1. OS MODULE
-// ==========================================
+const app = express();
 
-console.log("===== OS MODULE =====");
-
-console.log("Operating System:", os.platform());
-console.log("CPU Architecture:", os.arch());
-console.log("Computer Name:", os.hostname());
-console.log("Number of CPUs:", os.cpus().length);
-console.log("Total Memory:", os.totalmem());
-console.log("Free Memory:", os.freemem());
-
-
-// ==========================================
-// 2. PATH MODULE
-// ==========================================
-
-console.log("\n===== PATH MODULE =====");
-
-const filePath = path.join(__dirname, "files", "student.txt");
-
-console.log("Complete Path:", filePath);
-console.log("File Name:", path.basename(filePath));
-console.log("Directory:", path.dirname(filePath));
-console.log("File Extension:", path.extname(filePath));
-
-
-// ==========================================
-// 3. DNS MODULE
-// ==========================================
-
-console.log("\n===== DNS MODULE =====");
-
-dns.lookup("google.com", (error, address, family) => {
-
-    if (error) {
-        console.log("DNS Error:", error);
-        return;
-    }
-
-    console.log("Domain Name: google.com");
-    console.log("IP Address:", address);
-    console.log("IP Version:", family);
+app.get("/", (req, res) => {
+    res.send("Welcome to Home Page");
 });
 
-
-// ==========================================
-// 4. NET MODULE
-// ==========================================
-
-console.log("\n===== NET MODULE =====");
-
-const server = net.createServer((socket) => {
-
-    console.log("Client connected");
-
-    socket.write("Hello! You are connected to Node.js TCP Server.");
-
-    socket.on("data", (data) => {
-        console.log("Client says:", data.toString());
-    });
-
-    socket.on("end", () => {
-        console.log("Client disconnected");
-    });
-
+app.get("/about", (req, res) => {
+    res.send("Welcome to About Page");
 });
 
-server.listen(3000, () => {
-    console.log("TCP Server is running on port 3000");
+app.get("/contact", (req, res) => {
+    res.send("Welcome to Contact Page");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
